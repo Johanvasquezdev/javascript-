315aa2043a46ea7ab0484441a752c4bf8f473923
Johan Gabriel Vasquez <20251235@itla.edu.do>
2025-10-29 22:28:00 +0000

    convertir 3 en 1

--- prueba.js
+++ prueba.js
@@ -1,4 +1,6 @@
-function ConvertidorNota(nota) {
+
+// Ejercicio 1
+function convertidorNota(nota) {
   if (nota >= 90) {
     return 'A';
   } else if (nota >= 80) {
@@ -13,6 +15,55 @@ function ConvertidorNota(nota) {
 }
 
 console.log("Ejercicio 1: Notas");
-console.log(`Un 95 es: ${convertirCalificacion(95)}`);
-console.log(`Un 83 es: ${convertirCalificacion(83)}`);
-console.log(`Un 40 es: ${convertirCalificacion(40)}`);
+console.log(`Un 95 es: ${convertidorNota(95)}`);
+console.log(`Un 83 es: ${convertidorNota(73)}`);
+console.log(`Un 40 es: ${convertidorNota(40)}`);
+
+
+// Ejercicio 2
+const peliculas = [
+  "El Padrino",
+  "Hobbit",
+  "Interestelar",
+  "The Avengers",
+  "El Señor de los Anillos",
+  "Shrek 3"
+];
+
+console.log("Ejercicio 2: Películas");
+
+for (const pelicula of peliculas) {
+
+  if (pelicula.includes('Shrek')) {
+    continue; 
+  }
+
+  console.log(pelicula);
+}
+
+
+
+// Ejercicio 3
+function calcular(num1, num2, operador) {
+  switch (operador) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    case '/':
+      if (num2 === 0) {
+        return "Error manito, no puedes dividir entre cero";
+      }
+      return num1 / num2;
+      break
+  }
+}
+
+console.log("Ejercicio 3: Calculadora");
+console.log(`10 + 5 = ${calcular(10, 5, '+')}`);
+console.log(`10 - 5 = ${calcular(10, 5, '-')}`);
+console.log(`10 * 5 = ${calcular(10, 5, '*')}`);
+console.log(`10 / 5 = ${calcular(10, 5, '/')}`);
+console.log(`10 / 0 = ${calcular(10, 0, '/')}`);
